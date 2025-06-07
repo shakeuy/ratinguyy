9c2000dc414b70a7f689d6e88f1f409fed7362d5
shake7 <63744052+shakeuy@users.noreply.github.com>
2025-06-07 19:47:13 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,6 +3,8 @@ module.exports = {
   channels: [
     { id: 'UCp6X5jzfmwbOeclRArOi18g', name: 'Canal 4' },
     { id: 'UCMHQqhtbpxfcnPPYpryapHA', name: 'Canal 5' },
+    { id: 'UC8Ax1knAw4A1Gbwfg4chaKQ', name: 'Carve Deportiva' },
+    { id: 'UCm7HSve9w3PA7_C0VS_OeDw', name: 'Sport 890' },
     // ... otros canales
   ],
   updateInterval: 5 * 60 * 1000, // 5 minutos
